a420f88f0a1d23b15835d865e42b0d1f99d633ce
Hermawan S Sarjito <52445704+dark-hermes@users.noreply.github.com>
2023-06-13 20:56:44 +0700

    Update index.php

--- nadinee_siswi_sas_2023/index.php
+++ nadinee_siswi_sas_2023/index.php
@@ -32,7 +32,6 @@ if(isset($_POST['tombolsend'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>nadine</title>
     <link rel="stylesheet" href="index.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
     <nav>
@@ -77,14 +76,15 @@ while($row = $srcportofolio->fetch_assoc()){
 // Looping data untuk menampilkan
 foreach ($dataPorto as $porto) {
     ?>
-    <img src="asset/<?= $porto['foto']; ?> " alt="foto" />
+    <img src="asset/<?= $porto['foto']; ?>"/>
     <?php
 }
 ?>
         </div>
       </div>
+    </div>
 
-    <div class="about">
+    <div id="about">
       <div class="high">
         <h1 class="kutip">about</h1>
         <div class="bungkusdua">
@@ -96,7 +96,7 @@ foreach ($dataPorto as $porto) {
     </div>
 
 
-    <div class="kontak">
+    <div id="kontak">
       <div class="hub">
         <div>
           <h1 class="kutip">contact me</h1>
@@ -128,8 +128,4 @@ foreach ($dataPorto as $porto) {
       <p>copyright &copy; NADINE A 2007</p>
     </div>
   </body>
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
