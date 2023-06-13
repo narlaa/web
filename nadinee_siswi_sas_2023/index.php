274970699f1c410cb3be3a6d60b15a9ebc4e35d5
Hermawan S Sarjito <52445704+dark-hermes@users.noreply.github.com>
2023-06-13 20:51:43 +0700

    Update index.php

--- nadinee_siswi_sas_2023/index.php
+++ nadinee_siswi_sas_2023/index.php
@@ -32,6 +32,7 @@ if(isset($_POST['tombolsend'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>nadine</title>
     <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
     <nav>
@@ -58,27 +59,32 @@ if(isset($_POST['tombolsend'])){
       <h2 class="kutip"><?= $data_about['profesi']; ?></h2>
     </div>
 
-    <div id="porto">
       <div class="porto">
         <h1 class="kutip">portofolio</h1>
         <div class="bungkus"> 
           <?php    
 $sql = "SELECT * FROM portofolio";
 $srcportofolio = $conn->query($sql);
-$data_portofolio = $srcportofolio->fetch_assoc();
- {
-          ?>
-          <img src="asset/<?= $data_portofolio['foto']; ?>"/>
-          <img src="asset/<?= $data_portofolio['foto']; ?>"/>
-          <img src="asset/<?= $data_portofolio['foto']; ?>"/>
+// Buat variabel penampung data
+$dataPorto = [];
+
+// Looping setiap row dari query
+while($row = $srcportofolio->fetch_assoc()){
+  // Masukkan setiap row ke variabel data
+    $dataPorto[] = $row;
+}
+
+// Looping data untuk menampilkan
+foreach ($dataPorto as $porto) {
+    ?>
+    <img src="asset/<?= $porto['foto']; ?> " alt="foto" />
+    <?php
+}
+?>
         </div>
       </div>
-    </div>
-<?php
-                }
-                ?>
 
-    <div id="about">
+    <div class="about">
       <div class="high">
         <h1 class="kutip">about</h1>
         <div class="bungkusdua">
@@ -90,7 +96,7 @@ $data_portofolio = $srcportofolio->fetch_assoc();
     </div>
 
 
-    <div id="kontak">
+    <div class="kontak">
       <div class="hub">
         <div>
           <h1 class="kutip">contact me</h1>
@@ -122,4 +128,8 @@ $data_portofolio = $srcportofolio->fetch_assoc();
       <p>copyright &copy; NADINE A 2007</p>
     </div>
   </body>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
